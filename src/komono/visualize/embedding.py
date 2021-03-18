0a4792f7549d367508cbb41ef273c759ed5541b0
nagomiso <yuki-matsubara@c-fo.com>
2021-03-18 14:50:08 +0900

    Fix support data type

--- src/komono/visualize/embedding.py
+++ src/komono/visualize/embedding.py
@@ -4,14 +4,13 @@ from typing import List, Optional, Tuple, Union
 from matplotlib.colors import Colormap, Normalize
 from numpy import ndarray
 import pandas as pd
-from scipy.sparse import spmatrix
 import seaborn as sns
 from seaborn import JointGrid
 from sklearn.base import BaseEstimator
 
 
 def jointplot(
-    data: Union[pd.DataFrame, ndarray, spmatrix],
+    data: pd.DataFrame,
     embedding_transformer: BaseEstimator,
     kind: str = "scatter",
     color: Optional[Union[str, Colormap]] = None,
@@ -24,23 +23,23 @@ def jointplot(
     marginal_ticks: bool = False,
     joint_kws: Optional[dict] = None,
     marginal_kws: Optional[dict] = None,
-    hue: Optional[Union[str, list, ndarray]] = None,
+    hue: Optional[Union[str, List[Number], ndarray]] = None,
     palette: Optional[Union[str, list, dict, Colormap]] = None,
     hue_order: Optional[List[str]] = None,
     hue_norm: Optional[Union[tuple, Normalize]] = None,
     **kwargs
 ) -> Tuple[JointGrid, pd.DataFrame]:
-    if isinstance(data, pd.DataFrame):
-        X = data.values
-    else:
-        X = data
+    if isinstance(str, hue):
+        if data.columns.isin([hue]).sum():
+            data = data.set_index([hue])
+    X = data.values
     X_embedding: ndarray = embedding_transformer.fit_transform(X)
     df_embedding: pd.DataFrame = pd.DataFrame(
         data=X_embedding, index=data.index, columns=["x1", "x2"]
     )
     return (
         sns.jointplot(
-            data=df_embedding,
+            data=df_embedding.reset_index(hue),
             x="x1",
             y="x2",
             kind=kind,
